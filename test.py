c09fcf7b7f362a5940b16dce222cc3aee2c97d83
Jamsekun <agentjames44@gmail.com>
2025-09-22 12:00:43 +0800

    Clean repo: remove excess files and sync with local machine

--- test.py
+++
@@ -1,6 +0,0 @@
-from gpt4all import GPT4All
-
-model_path = r"C:\inventi-rag\models\mistral-7b-instruct-v0.1.Q4_K_S.gguf"
-model = GPT4All(model_path)
-response = model.generate("How are you?")
-print(response)
--- test8.py
+++
@@ -1,4 +0,0 @@
-import ollama
-
-response = ollama.chat(model="deepseek-r1:7b", messages=[{"role": "user", "content": "What floor is U-203 on?"}])
-print(response)
